9c7c978a12106921ccd0b4214e9c733f267efbfd
Adriel Hans <85242651+AdrielHansM@users.noreply.github.com>
2022-06-19 19:56:47 +0800

    'chore: fixed spacing'

--- src/pages/User/UserComponenents/EditProfile.tsx
+++ src/pages/User/UserComponenents/EditProfile.tsx
@@ -1,66 +1,95 @@
-import Navigation from "../../Components/Navigation"
-import '../../../assets/styles/EditProfile.css'
+import Navigation from "../../Components/Navigation";
+import "../../../assets/styles/EditProfile.css";
 
-export default function EditProfile(){
-
-    return(
-        <>
-        <Navigation/>
-
-        <div className="container rounded bg-white mt-5 mb-5"> 
-            <div className="row"> 
-            <div className="col-md-3 border-right"> 
-        </div> 
-
-        <div className="col-md-5 border-right"> 
-            <div className="p-3 py-5"> 
-                <div className="row mt-2"> 
-                        <div className="col-md-6">
-                        <label className="labels">Name</label>
-                        <input type="text" className="form-control" placeholder="first name"/>
-                    </div> 
-
-                        <div className="col-md-6">
-                        <label className="labels">Last Name</label>
-                        <input type="text" className="form-control" placeholder="last name"/>
-                    </div> 
-                </div> 
-
-            <div className="row mt-3"> 
-                <div className="col-md-12">
-                    <label className="labels">PhoneNumber</label>
-                    <input type="text" className="form-control" placeholder="Enter Phone Number"/>
-                </div> 
-
-                <div className="col-md-12">
-                    <label className="labels">Address</label>
-                    <input type="text" className="form-control" placeholder="Enter Address"/>
-                </div> 
-
-                <div className="col-md-12">
-                    <label className="labels">Email ID</label>
-                    <input type="text" className="form-control" placeholder="Enter Email"/>
-                </div> 
-            </div> 
-
-            <div className="row mt-3"> 
+export default function EditProfile() {
+  return (
+    <>
+      <Navigation />
+      <div className="container rounded bg-white mt-5 mb-5">
+        <div className="row">
+          <div className="col-md-3 border-right"></div>
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <div className="row mt-2">
                 <div className="col-md-6">
-                    <label className="labels">Country</label>
-                    <input type="text" className="form-control" placeholder="Country"/>
-                </div> 
-                <div className="col-md-6">
-                    <label className="labels">State/Region</label>
-                    <input type="text" className="form-control" placeholder="State"/>
-                </div> 
-            </div> 
+                  <label className="labels">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="first name"
+                  />
+                </div>
 
-            <div className="mt-5 text-center">
-                <button className="btn btn-primary profile-button" type="button">Save Profile</button>
-            </div> 
-            </div> 
+                <div className="col-md-6">
+                  <label className="labels">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="last name"
+                  />
+                </div>
+              </div>
+
+              <div className="row mt-3">
+                <div className="col-md-12">
+                  <label className="labels">PhoneNumber</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Phone Number"
+                  />
+                </div>
+
+                <div className="col-md-12">
+                  <label className="labels">Address</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Address"
+                  />
+                </div>
+
+                <div className="col-md-12">
+                  <label className="labels">Email ID</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Email"
+                  />
+                </div>
+              </div>
+
+              <div className="row mt-3">
+                <div className="col-md-6">
+                  <label className="labels">Country</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Country"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label className="labels">State/Region</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="State"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-5 text-center">
+                <button
+                  className="btn btn-primary profile-button"
+                  type="button"
+                >
+                  Save Profile
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
