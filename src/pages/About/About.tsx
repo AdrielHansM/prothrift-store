052636d6fd626d673ef07508bdb0d2e3cbb82b6e
Adriel Hans <85242651+AdrielHansM@users.noreply.github.com>
2022-06-29 22:24:52 +0800

    chore: adjust tab spacing

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -26,8 +26,8 @@ export default function About() {
         <p>
           ProThrift is a secondhand platform with a mission to empower people in
           their daily lives. "CHANGE LINE - We do this by providing leading
-          online marketplaces, building world-className media houses, and helping
-          great companies scale."
+          online marketplaces, building world-className media houses, and
+          helping great companies scale."
         </p>
       </div>
 
@@ -36,58 +36,82 @@ export default function About() {
           <div className="text-center">
             <div className="col-md-12">
               <h2>Our Team</h2>
-              <h3 className="caption gray">Meet the people who make awesome stuffs</h3>
+              <h3 className="caption gray">
+                Meet the people who make awesome stuffs
+              </h3>
             </div>
 
-          <div className="container2">
-          <div className="row-con">
+            <div className="container2">
+              <div className="row-con">
+                <div className="col-team">
+                  <div className="team-member">
+                    <figure>
+                      <img
+                        src="https://lh3.googleusercontent.com/a-/AOh14GiL-y6piLbVLUGYEtFPEikBCWFtmdfCYhVIn8bD=s360-p-rw-no"
+                        alt=""
+                        className="img-responsive"
+                      />
+                      <figcaption>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Recusandae asperiores mollitia.
+                        </p>
+                        <ul>
+                          <li>
+                            <a href="">
+                              <i className="fa-brands fa-facebook fa-2x"></i>
+                            </a>
+                          </li>
+                        </ul>
+                      </figcaption>
+                    </figure>
+                    <h4>Jade Jimenez</h4>
+                  </div>
+                </div>
 
-            <div className="col-team">
-              <div className="team-member">
-                <figure>
-                  <img src="https://lh3.googleusercontent.com/a-/AOh14GiL-y6piLbVLUGYEtFPEikBCWFtmdfCYhVIn8bD=s360-p-rw-no" alt="" className="img-responsive"/>
-                  <figcaption>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae asperiores mollitia.</p>
-                    <ul>
-                      <li><a href=""><i className="fa-brands fa-facebook fa-2x"></i></a></li>
-                    </ul>
-                  </figcaption>
-                </figure>
-                <h4>Jade Jimenez</h4>
-              </div>
-            </div>
-
-              <div className="col-team">
-                <div className="team-member">
-                  <figure>
-                    <img src="https://lh3.googleusercontent.com/a-/AOh14GiL-y6piLbVLUGYEtFPEikBCWFtmdfCYhVIn8bD=s360-p-rw-no" alt="" className="img-responsive"/>
-                    <figcaption>
-                      <p>Neque minima ea, a praesentium saepe nihil maxime quod esse numquam explicabo eligendi.</p>
-                      <ul>
-                        <li><a href=""><i className="fa-brands fa-facebook fa-2x"></i></a></li>
-                      </ul>
-                    </figcaption>
-                  </figure>
-                  <h4>Jianina Escalo</h4>
+                <div className="col-team">
+                  <div className="team-member">
+                    <figure>
+                      <img
+                        src="https://lh3.googleusercontent.com/a-/AOh14GiL-y6piLbVLUGYEtFPEikBCWFtmdfCYhVIn8bD=s360-p-rw-no"
+                        alt=""
+                        className="img-responsive"
+                      />
+                      <figcaption>
+                        <p>
+                          Neque minima ea, a praesentium saepe nihil maxime quod
+                          esse numquam explicabo eligendi.
+                        </p>
+                        <ul>
+                          <li>
+                            <a href="">
+                              <i className="fa-brands fa-facebook fa-2x"></i>
+                            </a>
+                          </li>
+                        </ul>
+                      </figcaption>
+                    </figure>
+                    <h4>Jianina Escalo</h4>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
       <div className="map">
-
-          <div >
-            <h1>Our Location</h1>
-            <h2>University of the Cordilleras, Gov. Pack Road</h2>
-          </div>
-          <iframe className="gmap"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2210.947130406117!2d120.59794280528612!3d16.409037850732762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xad71a188041ff97!2sUniversity%20Of%20The%20Cordilleras%20-%20Purchasing%20Office!5e0!3m2!1sen!2sph!4v1656098757826!5m2!1sen!2sph"></iframe>
+        <div>
+          <h1>Our Location</h1>
+          <h2>University of the Cordilleras, Gov. Pack Road</h2>
+        </div>
+        <iframe
+          className="gmap"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2210.947130406117!2d120.59794280528612!3d16.409037850732762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xad71a188041ff97!2sUniversity%20Of%20The%20Cordilleras%20-%20Purchasing%20Office!5e0!3m2!1sen!2sph!4v1656098757826!5m2!1sen!2sph"
+        ></iframe>
       </div>
 
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- src/pages/Shop/Women.tsx
+++ src/pages/Shop/Women.tsx
@@ -1,89 +1,92 @@
-import '../../assets/styles/Shop.css';
-import Navigation from '../Components/Navigation';
+import "../../assets/styles/Shop.css";
+import Navigation from "../Components/Navigation";
 import Footer from "../Components/Footer";
 
-
 export default function Women() {
-
   return (
     <>
-    <Navigation/>
-    <div>
-      <h2 className='productCateg'>Women's Clothes</h2>
-    </div>
-
-    <div className="product-container1">
-      <div className="product-card">
-          <div className="product-image">
-              <img src="/images/card4.png" className="product-thumb" alt=""/>
-              <button className="card-btn">Buy Product</button>
-          </div>
-          <div className="product-info">
-              <h2 className="product-brand">shorts</h2>
-              <p className="product-short-des">a short line about the cloth..</p>
-              <span className="price">$20</span><span className="actual-price">$40</span>
-          </div>
+      <Navigation />
+      <div>
+        <h2 className="productCateg">Women's Clothes</h2>
       </div>
-      <div className="product-card">
+      <div className="product-container1">
+        <div className="product-card">
           <div className="product-image">
-              <img src="/images/card4.png" className="product-thumb" alt=""/>
-              <button className="card-btn">Buy Product</button>
+            <img src="/images/card4.png" className="product-thumb" alt="" />
+            <button className="card-btn">Buy Product</button>
           </div>
           <div className="product-info">
-              <h2 className="product-brand">shorts</h2>
-              <p className="product-short-des">a short line about the cloth..</p>
-              <span className="price">$20</span><span className="actual-price">$40</span>
+            <h2 className="product-brand">shorts</h2>
+            <p className="product-short-des">a short line about the cloth..</p>
+            <span className="price">$20</span>
+            <span className="actual-price">$40</span>
           </div>
-      </div>   
-      <div className="product-card">
+        </div>
+        <div className="product-card">
           <div className="product-image">
-              <img src="/images/card4.png" className="product-thumb" alt=""/>
-              <button className="card-btn">Buy Product</button>
+            <img src="/images/card4.png" className="product-thumb" alt="" />
+            <button className="card-btn">Buy Product</button>
           </div>
           <div className="product-info">
-              <h2 className="product-brand">shorts</h2>
-              <p className="product-short-des">a short line about the cloth..</p>
-              <span className="price">$20</span><span className="actual-price">$40</span>
+            <h2 className="product-brand">shorts</h2>
+            <p className="product-short-des">a short line about the cloth..</p>
+            <span className="price">$20</span>
+            <span className="actual-price">$40</span>
           </div>
-      </div>                      
-    </div>
-    <div className="product-container1">
-      <div className="product-card">
+        </div>
+        <div className="product-card">
           <div className="product-image">
-              <img src="/images/card1.jpg" className="product-thumb" alt=""/>
-              <button className="card-btn">Buy Product</button>
+            <img src="/images/card4.png" className="product-thumb" alt="" />
+            <button className="card-btn">Buy Product</button>
           </div>
           <div className="product-info">
-              <h2 className="product-brand">shorts</h2>
-              <p className="product-short-des">a short line about the cloth..</p>
-              <span className="price">$20</span><span className="actual-price">$40</span>
+            <h2 className="product-brand">shorts</h2>
+            <p className="product-short-des">a short line about the cloth..</p>
+            <span className="price">$20</span>
+            <span className="actual-price">$40</span>
           </div>
+        </div>
       </div>
-      <div className="product-card">
+      <div className="product-container1">
+        <div className="product-card">
           <div className="product-image">
-              <img src="/images/card1.jpg" className="product-thumb" alt=""/>
-              <button className="card-btn">Buy Product</button>
+            <img src="/images/card1.jpg" className="product-thumb" alt="" />
+            <button className="card-btn">Buy Product</button>
           </div>
           <div className="product-info">
-              <h2 className="product-brand">shorts</h2>
-              <p className="product-short-des">a short line about the cloth..</p>
-              <span className="price">$20</span><span className="actual-price">$40</span>
+            <h2 className="product-brand">shorts</h2>
+            <p className="product-short-des">a short line about the cloth..</p>
+            <span className="price">$20</span>
+            <span className="actual-price">$40</span>
           </div>
-      </div>   
-      <div className="product-card">
+        </div>
+        <div className="product-card">
           <div className="product-image">
-              <img src="/images/card1.jpg" className="product-thumb" alt=""/>
-              <button className="card-btn">Buy Product</button>
+            <img src="/images/card1.jpg" className="product-thumb" alt="" />
+            <button className="card-btn">Buy Product</button>
           </div>
           <div className="product-info">
-              <h2 className="product-brand">shorts</h2>
-              <p className="product-short-des">a short line about the cloth..</p>
-              <span className="price">$20</span><span className="actual-price">$40</span>
+            <h2 className="product-brand">shorts</h2>
+            <p className="product-short-des">a short line about the cloth..</p>
+            <span className="price">$20</span>
+            <span className="actual-price">$40</span>
           </div>
-      </div>                    
-    </div>
+        </div>
+        <div className="product-card">
+          <div className="product-image">
+            <img src="/images/card1.jpg" className="product-thumb" alt="" />
+            <button className="card-btn">Buy Product</button>
+          </div>
+          <div className="product-info">
+            <h2 className="product-brand">shorts</h2>
+            <p className="product-short-des">a short line about the cloth..</p>
+            <span className="price">$20</span>
+            <span className="actual-price">$40</span>
+          </div>
+        </div>
+      </div>
 
-    <Footer />
+      <Footer />
     </>
-  )
+  );
 }
